17d1d32b91cbc489a48dac31cdc86a29f39be573
Enrique Walter Philippeaux <quique18c@gmail.com>
2019-08-20 08:48:01 -0300

    se programo exitosamente

--- esp_impresora.ino
+++ esp_impresora.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup()
+{
+    // put your setup code here, to run once:
+    pinMode(2, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // put your main code here, to run repeatedly:
 
+    digitalWrite(2, HIGH); // turn the LED on (HIGH is the voltage level)
+    delay(1000);                     // wait for a second
+    digitalWrite(2, LOW);  // turn the LED off by making the voltage LOW
+    delay(1000);                     // wait for a second
 }
