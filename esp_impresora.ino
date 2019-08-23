012e6ca6ee5b2f68b77d50a447e12b94aa447514
Enrique Walter Philippeaux <quique18c@gmail.com>
2019-08-23 08:56:35 -0300

    encoder andando.

--- esp_impresora.ino
+++ esp_impresora.ino
@@ -1,15 +1,27 @@
+#include <ESP32Encoder.h>
+
+#define ENCODER_A 33
+#define ENCODER_B 32
+
+ESP32Encoder encoder;
+
+
 void setup()
 {
     // put your setup code here, to run once:
     pinMode(2, OUTPUT);
+    // Attache pins for use as encoder pins
+	encoder.attachHalfQuad(ENCODER_A, ENCODER_B);
+    Serial.begin(115200);
+
+	// set starting count value
+	encoder.setCount(37);
+	Serial.println("Encoder Start = "+String((int32_t)encoder.getCount()));
 }
 
 void loop()
 {
     // put your main code here, to run repeatedly:
-
-    digitalWrite(2, HIGH); // turn the LED on (HIGH is the voltage level)
-    delay(1000);                     // wait for a second
-    digitalWrite(2, LOW);  // turn the LED off by making the voltage LOW
-    delay(1000);                     // wait for a second
+    Serial.println("Encoder count = "+String((int32_t)encoder.getCount()));
+	delay(100);
 }
