851be8f149da143f0c48fcd2748f057338a02598
lucasdavalle <48356540+lucasdavalle@users.noreply.github.com>
2019-10-18 11:58:27 -0300

    movimiento funcionando velocidad mas optima

--- esp_impresora2.ino
+++ esp_impresora2.ino
@@ -1,44 +1,18 @@
 
+
 #include <ESP32Encoder.h>
 
 #include <PID_v1.h>
 
-#include <math.h> // round, floor, ceil, trunc 
+#include <math.h> // round, floor, ceil, trunc
 
-#define ENCODER_A 33
-#define ENCODER_B 32
-#define MOTOR_A 27
-#define MOTOR_B 26
-#define MOTOR_A_PWM 1
-#define MOTOR_B_PWM 2
-
-#define MOTOR_PWM_FREQ 250
-#define MOTOR_PWM_RESOLUCION 8
-
-#define solenoide 13
+#include "header.h"
 
 ESP32Encoder encoder;
 
 double EJEX_POSICION_ENCODER_SETPOINT = 0;
 double EJEX_POSICION_ENCODER_ACTUAL;
-double EJEX_PID_OUTPUT; //pwm en una direccion u otra (-255 a 0 a +255)
-/*
-Parameters and what they do (sort of)
-P_Param:  the bigger the number the harder the controller pushes.
-I_Param:  the SMALLER the number (except for 0, which turns it off,)  the more quickly the controller reacts to load changes, but the greater the risk of oscillations.
-D_Param: the bigger the number  the more the controllr dampens oscillations (to the point where performance can be hindered)
-*/
-double CONS_KP = 0.3;
-double CONS_KI = 1;
-double CONS_KD = 0;
-
-
-uint16_t input_serial = 0;
-
-#define MOTOR_PWM_TOPEBAJO 125
-#define MOTOR_PWM_TOPEALTO 235
-
-PID myPID(&EJEX_POSICION_ENCODER_ACTUAL, &EJEX_PID_OUTPUT, &EJEX_POSICION_ENCODER_SETPOINT, CONS_KP, CONS_KI, CONS_KD, REVERSE);
+uint16_t resta;
 
 uint32_t lastMillis;
 
@@ -48,117 +22,131 @@ void setup()
     pinMode(2, OUTPUT);
     pinMode(MOTOR_A, OUTPUT);
     pinMode(MOTOR_B, OUTPUT);
-    pinMode(solenoide, OUTPUT);
 
     // Attache pins for use as encoder pins
     encoder.attachHalfQuad(ENCODER_A, ENCODER_B);
     Serial.begin(115200);
     EJEX_POSICION_ENCODER_SETPOINT = 0;
     EJEX_PONERACERO();
-    EJEX_PID_INICIAR();
 
     lastMillis = millis();
+
+    ledcAttachPin(MOTOR_A, MOTOR_A_PWM);
+    ledcAttachPin(MOTOR_B, MOTOR_B_PWM);
+    ledcSetup(MOTOR_A_PWM, MOTOR_PWM_FREQ, MOTOR_PWM_RESOLUCION);
+    ledcSetup(MOTOR_B_PWM, MOTOR_PWM_FREQ, MOTOR_PWM_RESOLUCION);
+    EJEX_POSICION_ENCODER_SETPOINT=450;
 }
 
 void loop()
 {
 
-      
-    if (millis() >= lastMillis + 500u)
+    if (millis() >= lastMillis + 500U)
     {
+        lastMillis = millis();
         Serial.println("ACT: " + String(EJEX_POSICION_ENCODER_ACTUAL));
         Serial.println("SET: " + String(EJEX_POSICION_ENCODER_SETPOINT));
-        Serial.println("OUT: " + String(EJEX_PID_OUTPUT));
-        lastMillis = millis();
-        if (EJEX_POSICION_ENCODER_ACTUAL<4000)
-        {
-            EJEX_POSICION_ENCODER_SETPOINT = EJEX_POSICION_ENCODER_SETPOINT+20;
-        }
-        else
-        {
-            EJEX_POSICION_ENCODER_SETPOINT = 0;
-        }
+        Serial.println("RESTA: " + String(resta));
+        //EJEX_POSICION_ENCODER_SETPOINT=EJEX_POSICION_ENCODER_SETPOINT+50;
         
-       
     }
-   
-    digitalWrite(solenoide,LOW);
+    if(EJEX_POSICION_ENCODER_SETPOINT>=3650)
+    {
+        EJEX_POSICION_ENCODER_SETPOINT=450;
+    }
 
     if (Serial.available())
     {
         String input = Serial.readStringUntil('\n');
         EJEX_POSICION_ENCODER_SETPOINT = input.toInt();
     }
-    
-    
-    EJEX_PID_COMPUTAR();
-
-
+    go_to();
 }
-
-void EJEX_PID_INICIAR()
+void go_to()
 {
-    ledcAttachPin(MOTOR_A, MOTOR_A_PWM);
-    ledcAttachPin(MOTOR_B, MOTOR_B_PWM);
-    ledcSetup(MOTOR_A_PWM, MOTOR_PWM_FREQ, MOTOR_PWM_RESOLUCION);
-    ledcSetup(MOTOR_B_PWM, MOTOR_PWM_FREQ, MOTOR_PWM_RESOLUCION);
-
-    myPID.SetMode(AUTOMATIC);
-    myPID.SetOutputLimits(0, 100);
-}
-
-void EJEX_PID_COMPUTAR()
-{
-    myPID.Compute();
     EJEX_POSICION_ENCODER_ACTUAL = encoder.getCount();
-    //hacer algo con la salida
+    resta = EJEX_POSICION_ENCODER_SETPOINT - EJEX_POSICION_ENCODER_ACTUAL;
+    //Serial.println("RESTA: " + String(resta));
 
-    /*
-    4300--------- 2000----------0
-    Si la coordenada X actual es mayor al setpoint de 2000 (Está a la izquierda de este)
-    Hay que mover el carrete hacia la derecha, con velocidad proporcional a la diferencia de distancia.
+    int b = 0;
+    if (resta < DISTANCIA_5)
+        b = 5;
+    else
+        b = 6;
+    if (resta < DISTANCIA_4)
+        b = 4;
+    if (resta < DISTANCIA_3)
+        b = 3;
+    if (resta < DISTANCIA_2)
+        b = 2;
+    if (resta <= DISTANCIA_ERROR)
+        b = 1;
 
-    La salida del PID se deberá mantener en 50, cuando el carrete este en su lugar (XACT = XSET)
-    Si se pasa a la izquierda, la salida del pid, sobrepasa 50.
-    Caso contrario, pasa or debajo de 50.
-
-    */
-
-    //ledcWrite(MOTOR_A_PWM, input.toInt());
-
-    //Sacarle el decimal a la salida:
-
-    int sindecimal = EJEX_PID_OUTPUT;
-    EJEX_PID_OUTPUT = sindecimal;
-
-    if (EJEX_PID_OUTPUT > 50) //Se paso para la izquierda
+    if (EJEX_POSICION_ENCODER_ACTUAL < EJEX_POSICION_ENCODER_SETPOINT)
     {
-        ledcWrite(MOTOR_B_PWM, myMap(EJEX_PID_OUTPUT, 50, 100, MOTOR_PWM_TOPEBAJO, MOTOR_PWM_TOPEALTO));
-        ledcWrite(MOTOR_A_PWM, 0);
+        switch (b)
+        {
+        case 1:
+            ledcWrite(MOTOR_A_PWM, 0);
+            ledcWrite(MOTOR_B_PWM, 0);
+            break;
+        case 2:
+            ledcWrite(MOTOR_A_PWM, MOTOR_PWM_1);
+            ledcWrite(MOTOR_B_PWM, 0);
+            break;
+        case 3:
+            ledcWrite(MOTOR_A_PWM, MOTOR_PWM_2);
+            ledcWrite(MOTOR_B_PWM, 0);
+            break;
+        case 4:
+            ledcWrite(MOTOR_A_PWM, MOTOR_PWM_3);
+            ledcWrite(MOTOR_B_PWM, 0);
+            break;
+        case 5:
+            ledcWrite(MOTOR_A_PWM, MOTOR_PWM_4);
+            ledcWrite(MOTOR_B_PWM, 0);
+            break;
+        case 6:
+            ledcWrite(MOTOR_A_PWM, MOTOR_PWM_TOPEALTO);
+            ledcWrite(MOTOR_B_PWM, 0);
+            break;
+        }
     }
-
-    if (EJEX_PID_OUTPUT < 50) //Se paso para la derecha
+    if (EJEX_POSICION_ENCODER_ACTUAL > EJEX_POSICION_ENCODER_SETPOINT)
     {
-        ledcWrite(MOTOR_A_PWM, myMap(EJEX_PID_OUTPUT, 50, 0, MOTOR_PWM_TOPEBAJO, MOTOR_PWM_TOPEALTO));
-        ledcWrite(MOTOR_B_PWM, 0);
+        switch (b)
+        {
+        case 1:
+            ledcWrite(MOTOR_B_PWM, 0);
+            ledcWrite(MOTOR_A_PWM, 0);
+            break;
+        case 2:
+            ledcWrite(MOTOR_B_PWM, MOTOR_PWM_1);
+            ledcWrite(MOTOR_A_PWM, 0);
+            break;
+        case 3:
+            ledcWrite(MOTOR_B_PWM, MOTOR_PWM_2);
+            ledcWrite(MOTOR_A_PWM, 0);
+            break;
+        case 4:
+            ledcWrite(MOTOR_B_PWM, MOTOR_PWM_3);
+            ledcWrite(MOTOR_A_PWM, 0);
+            break;
+        case 5:
+            ledcWrite(MOTOR_B_PWM, MOTOR_PWM_4);
+            ledcWrite(MOTOR_A_PWM, 0);
+            break;
+        case 6:
+            ledcWrite(MOTOR_B_PWM, MOTOR_PWM_TOPEALTO);
+            ledcWrite(MOTOR_A_PWM, 0);
+            break;
+        }
     }
-
-    if (EJEX_PID_OUTPUT == 50) //No hacer nada.
-    {
-        ledcWrite(MOTOR_B_PWM, 0);
-        ledcWrite(MOTOR_B_PWM, 0);
-    }
-}
-
-int myMap(double x, double in_min, double in_max, double out_min, double out_max)
-{
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void EJEX_PONERACERO()
 {
     //ir a 0.
-
     digitalWrite(MOTOR_A, LOW);
     digitalWrite(MOTOR_B, HIGH);
     delay(3500);
---
+++ header.h
@@ -0,0 +1,34 @@
+
+
+#include <ESP32Encoder.h>
+
+#include <PID_v1.h>
+
+#include <math.h> // round, floor, ceil, trunc 
+
+#define ENCODER_A 32
+#define ENCODER_B 33
+#define MOTOR_A 27
+#define MOTOR_B 26
+#define MOTOR_A_PWM 1
+#define MOTOR_B_PWM 2
+
+#define MOTOR_PWM_FREQ 250
+#define MOTOR_PWM_RESOLUCION 8
+
+#define SOLENOIDE 13
+#define MOTOR_PWM_TOPEBAJO 125
+#define MOTOR_PWM_TOPEALTO 235
+
+#define NUMERO_POSICIONES_X 70
+#define NUMERO_POSICIONES_Y 70
+
+#define DISTANCIA_ERROR 2
+#define DISTANCIA_2 25
+#define DISTANCIA_3 50
+#define DISTANCIA_4 100
+#define DISTANCIA_5 150
+#define MOTOR_PWM_1 165
+#define MOTOR_PWM_2 180
+#define MOTOR_PWM_3 195
+#define MOTOR_PWM_4 225
